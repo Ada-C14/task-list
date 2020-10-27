b503406901602f4123aa15bbbc05ca5682cd797f
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-10-26 21:57:16 -0700

    for baseline setup

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,11 @@
+TASKS = [
+  { to_do_list: "Hidden Figures", due_date: "Margot Lee Shetterly"},
+  { to_do_list: "Practical Object-Oriented Design in Ruby", due_date: "Sandi Metz"},
+  { to_do_list: "Kindred", due_date: "Octavia E. Butler"}
+]
+
+class TasksController < ApplicationController
+  def index
+    @tasks = TASKS
+  end
+end
