e04a08281da1957d5a9750d98077c1eb2db7d045
Tram Bui <pbui17@cmc.edu>
2020-10-27 13:15:24 -0500

    completed wave 1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,22 @@
-TASKS = ["complete homework", "pick up groceries", "laundry", "meal prep", "pick up Mom"]
+# TASKS = ["complete homework", "pick up groceries", "laundry", "meal prep", "pick up Mom"]
 
 # TASKS = []
 
 class TasksController < ApplicationController
 
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
+
+  def show
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+
+    # @task = TASKS[task_id]
+    if @task.nil?
+      head :redirect
+      return
+    end
+  end
+
 end
