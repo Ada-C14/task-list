ca23e7a4b548523bf17be6b6ac14a563142c0e25
Anna Kim <anna.kim93@gmail.com>
2020-10-27 00:39:05 -0400

    created an ERB view to display the tasks from the controller action; minor edits to test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,14 +1,15 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
+
 require 'rails/test_help'
-      require "minitest/rails"
-      require "minitest/reporters"  # for Colorized output
-      #  For colorful output!
-      Minitest::Reporters.use!(
-        Minitest::Reporters::SpecReporter.new,
-        ENV,
-        Minitest.backtrace_filter
-      )
+require "minitest/rails"
+require "minitest/reporters"  # for Colorized output
+#  For colorful output!
+Minitest::Reporters.use!(
+    Minitest::Reporters::SpecReporter.new,
+    ENV,
+    Minitest.backtrace_filter
+)
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
