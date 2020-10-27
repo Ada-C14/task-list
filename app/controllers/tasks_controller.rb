165ba5b9ee69db2f5d0b2919800d476e695ff25f
Iris Lux <iris.lux0@gmail.com>
2020-10-27 01:19:08 -0700

    altered date_completed key to completed_at

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,8 @@
 require "time"
 TASKS = [
-    { name: "Do laundry", description: "Do the damn laundry", date_completed: Time.now},
-    { name: "Do dishes", description: "Do the damn dishes", date_completed: Time.new(1989)},
-    { name: "Cry in bed", description: "Waaahhh", date_completed: Time.new(1969)},
+    { name: "Do laundry", description: "Do the damn laundry", completed_at: Time.now},
+    { name: "Do dishes", description: "Do the damn dishes", completed_at: Time.new(1989)},
+    { name: "Cry in bed", description: "Waaahhh", completed_at: Time.new(1969)},
 ]
 class TasksController < ApplicationController
   def index
