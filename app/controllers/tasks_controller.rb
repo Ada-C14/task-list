e2b9c5a9e203298b0dcd8cf916992b5567637a28
Iris Lux <iris.lux0@gmail.com>
2020-10-26 23:52:01 -0700

    added controller for tasks, and created index method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,11 @@
+require "time"
+TASKS = [
+    { name: "Do laundry", description: "Do the damn laundry", date_completed: Time.now},
+    { name: "Do dishes", description: "Do the damn dishes", date_completed: Time.new(1989)},
+    { name: "Cry in bed", description: "Waaahhh", date_completed: Time.new(1969)},
+]
 class TasksController < ApplicationController
+  def index
+    @tasks = TASKS
+  end
 end
