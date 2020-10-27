752596528ef7f07a7c9b6157efd200a8d0da262e
Ayesha Asif <ayaseef@gmail.com>
2020-10-27 16:14:50 -0700

    TasksController: Index tests passed

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,14 @@
+TASKS = [ 
+  { description: 'Make dinner', completed: false },
+  { description: 'Drink my tea', completed: false },
+  { description: 'Pay tuition', completed: false }
+].freeze
+
+
+
+
+class TasksController < ApplicationController
+  def index
+    @tasks = TASKS
+  end
+end
