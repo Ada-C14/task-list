2d7913a73247cd4e0c86e1e949c3947e055213f6
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-10-26 19:54:17 -0700

    Run rails new and create Tasks controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,10 @@
+TASKS = [
+    { name: 'Go to work', description: 'Ada', completed_at: 'Oct. 26, 2020'},
+    { name: 'Buy a costume', description: 'Halloween', completed_at: 'Oct. 27, 2020'}
+]
+
+class TasksController < ApplicationController
+  def index
+    @tasks = TASKS
+  end
+end
