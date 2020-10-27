ad62a51a0e80b3ac5768705bc5066cdf10096f3b
Pauline Chane <pauline.chane@gmail.com>
2020-10-26 17:18:26 -0700

    added Task model and 2 Task items to database; updated ERB view and controller; wave 1 complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,6 @@
-# temp variable for tasks until we use database
-TASKS = ["Go to QFC", "Send Snail Mail", "Finish Design Document"]
 # tasks controller class
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 end
