48df36a95d580ab4644f8508c241697d9ff30cd0
Iris Lux <iris.lux0@gmail.com>
2020-10-27 23:40:18 -0700

    added data to database and revised controller so it references db and not hardcode

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,13 @@
 require "time"
-TASKS = [
-    { name: "Do laundry", description: "Do the damn laundry", completed_at: Time.now},
-    { name: "Do dishes", description: "Do the damn dishes", completed_at: Time.new(1989)},
-    { name: "Cry in bed", description: "Waaahhh", completed_at: Time.new(1969)},
-]
+
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 
   def show
     task_id = params[:id].to_i
-    @task = TASKS[task_id]
+    @task = Task.find(task_id)
     if @task.nil?
       head :not_found
       return
