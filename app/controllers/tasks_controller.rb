8299ca402813ca33bbd0656cd0283e2cb7d4e39a
Genevieve Neely <genevieve.hood@gmail.com>
2020-10-27 19:05:46 -0700

    implement task#show, update views, routes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,31 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: task_id)
+    if @task.nil?
+      redirect_to tasks_path
+      return
+    end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(
+        name: params[:task][:name],
+        description: params[:task][:description],
+        completed_at: params[:task][:completed_at]
+      )
+    if @task.save
+      redirect_to task_path(@task.id)
+    else
+      render :new
+      return
+    end
+  end
 end
