164cf6baecebb20e2a14adb29c9dde717b963a03
Rachael Gomez <gomezrc1220@gmail.com>
2020-10-28 16:38:13 -0700

    did steps in wave 1. Tests aren't passing, not sure why. Things look correct on the webpage.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,14 +2,6 @@ TASKS = ["be cool", "be calm", "be collected", "lose your ever-loving mind"]
 
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
-  end
-  def show
-    task_id = params[:id].to_i
-    @task = TASKS[task_id]
-    if @task.nil?
-      head :not_found
-      return
-    end
+    @tasks = Task.all
   end
 end
