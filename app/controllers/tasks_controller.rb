d0dcd5adde45309dbd8448e29d1c7ae8ae96a3c9
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-10-28 00:12:18 -0700

    update controller and index based on Model for wave1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,15 @@
-TASKS = [
-  { to_do_list: "Hidden Figures", due_date: "Margot Lee Shetterly"},
-  { to_do_list: "Practical Object-Oriented Design in Ruby", due_date: "Sandi Metz"},
-  { to_do_list: "Kindred", due_date: "Octavia E. Butler"}
-]
+# TASKS = [
+#   { to_do_list: "Hidden Figures", due_date: "Margot Lee Shetterly"},
+#   { to_do_list: "Practical Object-Oriented Design in Ruby", due_date: "Sandi Metz"},
+#   { to_do_list: "Kindred", due_date: "Octavia E. Butler"}
+# ]
 
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
+  end
+
+  def show
+    
   end
 end
