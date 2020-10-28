907e3bec538c94063388e327c2c1d598a5e92e0f
MonaRahmani <monarahmanimehr@gmail.com>
2020-10-28 09:11:34 -0700

    html/views edited

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,25 @@
+# TASKS = [
+#     {description: "pay bills", date: "10/12", priority: "high"},
+#     {description: "shopping", date: "10/25", priority: "low"},
+#     {description: "homework", date: "10/30", priority: "high"}
+# ]
+
+
 class TasksController < ApplicationController
-  TASKS = [
-      {description: "pay bills", date: "10/12", priority: "high"},
-      {description: "shopping", date: "10/25", priority: "low"},
-      {description: "homework", date: "10/30", priority: "high"}
-  ]
+
 
   def index
-    @tasks = TASKS
+    @tasks = Book.all
+  end
+
+
+  def show
+    book_id = params[:id]
+    @book = Book.find_by(id: book_id)
+    if @book.nil?
+      head :not_found
+      return
+    end
   end
 
 end
