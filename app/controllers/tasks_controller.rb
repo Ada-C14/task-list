7b309e11f590dca449a2ed2024d78904e2499b1e
roshni-patel <rpatel01@wesleyan.edu>
2020-10-28 14:20:39 -0500

    created show action, route, ERB view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,13 @@ class TasksController < ApplicationController
     def index 
         @tasks = Task.all  
     end
+
+    def show
+        task_id = params[:id].to_i
+        @task = Task.find_by(id: task_id)
+        if @task.nil?
+          redirect_to action: "index" 
+          return
+        end
+    end
 end
