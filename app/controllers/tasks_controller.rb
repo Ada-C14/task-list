b513c7702a3ce6d5209aefd42c4fa3e8f6007a60
Emily Nagai <emnagai89@gmail.com>
2020-10-28 16:32:10 -0700

    Wave 2 complete and passing tests

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,8 +8,26 @@ class TasksController < ApplicationController
     @task = Task.find_by(id: task_id)
 
     if @task.nil?
-      head :redirect
+      head 302
       return
+    end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(
+        name: params[:task][:name], #what is params again? the request parameter built in rails?
+        description: params[:task][:description],
+        completed_at: params[:task][:completed_at]
+    )
+
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new, :bad_request
     end
   end
 end
