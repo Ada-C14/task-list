cf91e671a840d7ee6efbab5626f3eebde70ba18e
Schanen Ryan <schanen.r@gmail.com>
2020-10-27 21:43:47 -0700

    baseline requirements

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,14 @@
+
+TASKS = [
+    {todo: "Walk the dog"},
+    {todo: "Make the mung beans"},
+    {todo: "Go to the wine store"},
+    {todo: "Eat a vegetable"},
+    {todo: "Call your girl"}
+]
+
 class TasksController < ApplicationController
+  def index
+    @tasks = TASKS
+  end
 end
