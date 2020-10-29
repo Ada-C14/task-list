826fe930949d66609794720769d67c1c69591382
Ayesha Asif <ayaseef@gmail.com>
2020-10-28 23:07:28 -0700

    Wave 2 completed, one create test failing, to be fixed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,11 +4,68 @@ TASKS = [
   { description: 'Pay tuition', completed: false }
 ].freeze
 
-
-
-
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  # def show
+  #   @task = Task.find(params[:id])
+  #   if @task.nil?
+  #     redirect_to action: "index"
+  #   end
+  # end
+  #
+
+  def show
+    task_id = params[:id].to_i
+    if !Task.exists?(task_id)
+      redirect_to root_path
+      return
+    end
+    @task = Task.find(params[:id].to_i)
+    if @task.nil?
+      redirect_to root_path
+      return
+    end
+    render :show
+  end
+
+  # def show
+  #   task_id = params[:id].to_i
+  #   @task = Task[task_id]
+  #   if @task.nil?
+  #     redirect_to root_path
+  #     return
+  #   end
+  # end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description]) #instantiate a new task
+    if @task.save # save returns true if the database insert succeeds
+      redirect_to root_path # go to the index so we can see the book in the list
+      return
+    else # save failed :(
+    render :new # show the new book form view again
+    return
+    end
+  end
+
+  # app/controllers/tasks_controller.rb
+  # def destroy
+  #   task_id = params[:id]
+  #   @task = Task.find_by(id: task_id)
+  #
+  #   if @task
+  #     @task.destroy
+  #     redirect_to tasks_path
+  #   else
+  #     render :notfound, status: :not_found
+  #   end
+  # end
+
 end
--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -5,61 +5,61 @@ describe TasksController do
     Task.create name: "sample task", description: "this is an example for a test",
     completed_at: Time.now + 5.days
   }
-  
+
   # Tests for Wave 1
   describe "index" do
     it "can get the index path" do
       # Act
       get tasks_path
-      
+
       # Assert
       must_respond_with :success
     end
-    
+
     it "can get the root path" do
       # Act
       get root_path
-      
+
       # Assert
       must_respond_with :success
     end
   end
-  
+
   # Unskip these tests for Wave 2
   describe "show" do
     it "can get a valid task" do
       # Act
       get task_path(task.id)
-      
+
       # Assert
       must_respond_with :success
     end
-    
+
     it "will redirect for an invalid task" do
       # Act
       get task_path(-1)
-      
+
       # Assert
       must_respond_with :redirect
     end
   end
-  
+
   describe "new" do
     it "can get the new task page" do
 
-      
+
       # Act
       get new_task_path
-      
+
       # Assert
       must_respond_with :success
     end
   end
-  
+
   describe "create" do
     it "can create a new task" do
 
-      
+
       # Arrange
       task_hash = {
         task: {
@@ -68,34 +68,34 @@ describe TasksController do
           completed_at: nil,
         },
       }
-      
+
       # Act-Assert
       expect {
         post tasks_path, params: task_hash
       }.must_change "Task.count", 1
-      
+
       new_task = Task.find_by(name: task_hash[:task][:name])
       expect(new_task.description).must_equal task_hash[:task][:description]
       expect(new_task.completed_at).must_equal task_hash[:task][:completed_at]
-      
+
       must_respond_with :redirect
       must_redirect_to task_path(new_task.id)
     end
   end
-  
+
   # Unskip and complete these tests for Wave 3
   describe "edit" do
     it "can get the edit page for an existing task" do
 
       # Your code here
     end
-    
+
     it "will respond with redirect when attempting to edit a nonexistant task" do
 
       # Your code here
     end
   end
-  
+
   # Uncomment and complete these tests for Wave 3
   describe "update" do
     # Note:  If there was a way to fail to save the changes to a task, that would be a great
@@ -103,18 +103,38 @@ describe TasksController do
     it "can update an existing task" do
       # Your code here
     end
-    
+
     it "will redirect to the root page if given an invalid id" do
       # Your code here
     end
   end
-  
+
   # Complete these tests for Wave 4
   describe "destroy" do
-    # Your tests go here
-    
+    it "can destroy a model" do
+      # Arrange
+      new_task = Task.new name: "Practical Object Oriented Programming in Ruby", description: "Sandi Metz"
+
+      new_task.save
+      id = new_task.id
+
+      # Act
+      expect {
+        delete task_path(id)
+
+        # Assert
+      }.must_change 'Task.count', -1
+
+      new_task = Task.find_by(name: "Practical Object Oriented Programming in Ruby")
+
+      expect(new_task).must_be_nil
+
+      must_respond_with :redirect
+      must_redirect_to task_path
+    end
+
   end
-  
+
   # Complete for Wave 4
   describe "toggle_complete" do
     # Your tests go here
