498737099daed138d7675e615b154d02a0859322
Schanen Ryan <schanen.r@gmail.com>
2020-10-28 21:27:17 -0700

    added completed_at and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_035552) do
+ActiveRecord::Schema.define(version: 2020_10_29_042031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_10_29_035552) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "completed_at"
   end
 
 end
