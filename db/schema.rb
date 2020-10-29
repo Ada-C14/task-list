a36ea40bb206903b7eab2c0086f764a79f0ae27f
Christabel Sebastian <christabel.escarez@gmail.com>
2020-10-29 11:05:48 -0700

    generated task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_235709) do
+ActiveRecord::Schema.define(version: 2020_10_29_175209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
