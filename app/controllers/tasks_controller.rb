89dfc3cb38c5c5807a2a1f3a12f6802c8489286a
gessica <gessicamohr18@gmail.com>
2020-10-29 12:58:28 -0700

    Activer Record: fixed TasksController, def index, def show. Fixed views-index and views-show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,17 @@
-TASKS = [ "Buy groceries", "Take out the trash", "Take the dog for a walk", "Do laundry"  ]
+# TASKS = [ "Buy groceries", "Take out the trash", "Take the dog for a walk", "Do laundry"  ]
 
 
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
+  end
+
+  def show
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+    if @task.nil?
+      head :not_found
+      return
+    end
   end
 end
