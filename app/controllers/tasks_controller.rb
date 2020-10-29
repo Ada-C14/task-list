bcbab66ab26878ef679b014e2dae788ac0cab679
Marj E <marj@marjorieetienne.com>
2020-10-28 22:51:51 -0700

    completed wave 1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,5 @@
-#temp
-TASKS = [
-    {title: "clean the bathroom"},
-    {title: "clean the kitchen"},
-    {title: "do laundry"}
-]
-
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 end
