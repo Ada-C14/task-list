602cc435faffc170e686066947d39844a7d7b30f
Ayesha Asif <ayaseef@gmail.com>
2020-10-29 12:26:14 -0700

    Wave 2 completed with all tests passing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,14 +9,6 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
-  # def show
-  #   @task = Task.find(params[:id])
-  #   if @task.nil?
-  #     redirect_to action: "index"
-  #   end
-  # end
-  #
-
   def show
     task_id = params[:id].to_i
     if !Task.exists?(task_id)
@@ -31,41 +23,32 @@ class TasksController < ApplicationController
     render :show
   end
 
-  # def show
-  #   task_id = params[:id].to_i
-  #   @task = Task[task_id]
-  #   if @task.nil?
-  #     redirect_to root_path
-  #     return
-  #   end
-  # end
-
   def new
     @task = Task.new
   end
 
   def create
-    @task = Task.new(name: params[:task][:name], description: params[:task][:description]) #instantiate a new task
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description], completed_at: params[:task][:completed_at]) #instantiate a new task
     if @task.save # save returns true if the database insert succeeds
-      redirect_to root_path # go to the index so we can see the book in the list
+      redirect_to task_path(@task.id) # go to the index so we can see the task in the list
       return
     else # save failed :(
-    render :new # show the new book form view again
+    render :new # show the new task form view again
     return
     end
   end
 
   # app/controllers/tasks_controller.rb
-  # def destroy
-  #   task_id = params[:id]
-  #   @task = Task.find_by(id: task_id)
-  #
-  #   if @task
-  #     @task.destroy
-  #     redirect_to tasks_path
-  #   else
-  #     render :notfound, status: :not_found
-  #   end
-  # end
+  def destroy
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+
+    if @task
+      @task.destroy
+      redirect_to task_path(@task.id)
+    else
+      render :notfound, status: :not_found
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # verb 'path', to: 'controller#action'
-  get '/tasks', to: 'tasks#index'
-  # resources :tasks, only: [:index, :show]
-  root to: 'tasks#index'
-
-  # Routes that operate on the task collection
-  get '/tasks/new', to: 'tasks#new', as: :new_task
-  post '/tasks', to: 'tasks#create'
-
-  # Routes that operate on individual tasks
-  get '/tasks/:id', to: 'tasks#show', as: :task
-  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch '/tasks/:id', to: 'tasks#update'
-  delete '/tasks/:id', to: 'tasks#destroy'
+  # get '/tasks', to: 'tasks#index'
+  root to: "tasks#index"
+  resources :tasks
+  # root to: 'tasks#index'
+  #
+  # # Routes that operate on the task collection
+  # get '/tasks/new', to: 'tasks#new', as: :new_task
+  # post '/tasks', to: 'tasks#create'
+  #
+  # # Routes that operate on individual tasks
+  # get '/tasks/:id', to: 'tasks#show', as: :task
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch '/tasks/:id', to: 'tasks#update'
+  # delete '/tasks/:id', to: 'tasks#destroy'
+  #
+  #
+  # # get    "/books"          , to: "books#index",   as: :book
+  # # post   "/books"          , to: "books#create"
+  # # get    "/books/new"      , to: "books#new",     as: :new_book
+  # # get    "/books/:id"      , to: "books#show",    as: :book
+  # # patch  "/books/:id"      , to: "books#update"
+  # # put    "/books/:id"      , to: "books#update"
+  # # delete "/books/:id"      , to: "books#destroy"
+  # # get    "/books/:id/edit" , to: "books#edit",    as: :edit_book
 end
