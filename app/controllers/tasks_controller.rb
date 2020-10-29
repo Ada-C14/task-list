2f2d0eaf4b049e1085c3d6e2647d321ac10ea2e4
Schanen Ryan <schanen.r@gmail.com>
2020-10-28 21:44:16 -0700

    updates tasks controller to link database

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,14 @@
-
-TASKS = [
-    {todo: "Walk the dog"},
-    {todo: "Make the mung beans"},
-    {todo: "Go to the wine store"},
-    {todo: "Eat a vegetable"},
-    {todo: "Call your girl"}
-]
+#
+# TASKS = [
+#     {todo: "Walk the dog"},
+#     {todo: "Make the mung beans"},
+#     {todo: "Go to the wine store"},
+#     {todo: "Eat a vegetable"},
+#     {todo: "Call your girl"}
+# ]
 
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 end
