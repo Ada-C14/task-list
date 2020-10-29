dcf237d68a85291aa81ead340ad3214b664d83ab
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-10-28 21:23:58 -0700

    update tasks controller and view to use migration

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
-TASKS = [
-    { name: 'Go to work', description: 'Ada', completed_at: 'Oct. 26, 2020'},
-    { name: 'Buy a costume', description: 'Halloween', completed_at: 'Oct. 27, 2020'},
-    { name: 'Water plants', description: 'Chores', completed_at: 'Oct. 28, 2020'}
-]
+# TASKS = [
+#     { name: 'Go to work', description: 'Ada', completed_at: 'Oct. 26, 2020'},
+#     { name: 'Buy a costume', description: 'Halloween', completed_at: 'Oct. 27, 2020'},
+#     { name: 'Water plants', description: 'Chores', completed_at: 'Oct. 28, 2020'}
+# ]
 
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 end
