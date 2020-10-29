13e149fd54e484d278275f6107a7841b7db2bf22
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-10-28 20:21:15 -0700

    add root to routes.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 TASKS = [
     { name: 'Go to work', description: 'Ada', completed_at: 'Oct. 26, 2020'},
-    { name: 'Buy a costume', description: 'Halloween', completed_at: 'Oct. 27, 2020'}
+    { name: 'Buy a costume', description: 'Halloween', completed_at: 'Oct. 27, 2020'},
+    { name: 'Water plants', description: 'Chores', completed_at: 'Oct. 28, 2020'}
 ]
 
 class TasksController < ApplicationController
