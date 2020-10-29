d57414bd5374d9b656a3a36ae22a0bbca6b0c6cb
Stacy <lundquist.stacy@gmail.com>
2020-10-28 22:27:24 -0700

    Wave 1: added task via Task.new, added task via Task.create; update views and controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,6 +6,11 @@ TASKS = [
 
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
+  end
+
+  def show
+    task_id = params[:id]
+    @task = Task.find_by(id:task_id)
   end
 end
