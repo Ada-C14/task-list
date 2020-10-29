68d1e36af72adebfa18bffa1363f8aee72fb6123
Ringo Alcock <ringo.alcock@gmail.com>
2020-10-29 19:23:38 -0400

    make show action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,13 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: task_id)
+
+    if @task.nil?
+      redirect_to tasks_path and return
+    end
+  end
 end
