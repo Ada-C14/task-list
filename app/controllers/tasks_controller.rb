fa31a9cfb8ce5ca060c0a81d538ec5c09ed91ff5
Pauline Chane <pauline.chane@gmail.com>
2020-10-29 16:41:15 -0700

    added show to task controller and view for individual tasks; updated pages with skeleton of functionalities to be added throughout project

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,25 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    begin
+      @task = Task.find(params[:id])
+    rescue StandardError
+      redirect_to root_path
+    end
+  end
+
+  def create
+    @task = Task.new(name: params[:task][:name],
+                     description: params[:task][:description],
+                     completed_at: params[:task][:completed_at]) #instantiate a new book
+    if @task.save # save returns true if the database insert succeeds
+      redirect_to root_path # go to the index so we can see the book in the list
+      return
+    else # save failed :(
+    render :new # show the new book form view again
+    return
+    end
+  end
 end
