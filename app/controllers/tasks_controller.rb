6b8ee01c3f316c6576a8853557c77606871ed6b5
roshni-patel <rpatel01@wesleyan.edu>
2020-10-29 00:59:20 -0500

    Updated task list to link to show action, added option to create new task using forms

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,4 +11,31 @@ class TasksController < ApplicationController
           return
         end
     end
+
+    # def update 
+    # end
+
+    # def edit 
+    # end
+
+    # def destroy
+    # end
+
+    def new
+        @task = Task.new
+    end
+
+    def create
+        @task = Task.new(name: params[:task][:name],
+            description: params[:task][:description],
+            completed_at: params[:task][:completed_at]) 
+
+        if @task.save 
+            redirect_to task_path(@task.id)
+            return
+        else
+            render :new, :bad_request
+            return 
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tasks
   root to: 'tasks#index' 
-  get '/tasks', to: 'tasks#index'
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  #get '/tasks', to: 'tasks#index', as: 'tasks' # list all tasks
+  #get '/tasks/new', to: 'tasks#new', as: 'new_task' # gets form for new task
+  #get '/tasks/:id', to: 'tasks#show', as: 'task' # show details for one task
+  #get '/tasks/new', to: 'tasks#new', as: 'new_task' # SHOULD GO BEFORE ID, EXAMPLE OF HOW ORDER MATTERS
+  #post '/tasks', to: 'tasks#create' # create a task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task' # brings up form to edit a task
+  # patch '/tasks/:id', to: 'tasks#update' # update an existing task
+  # delete '/tasks/:id', to: 'tasks#destroy' # destroy a given task
 
 end
