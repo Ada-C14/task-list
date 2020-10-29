7ed934073b876629fb26266c53855ec058794015
Noor B.A <noor.bahr.aluloom@gmail.com>
2020-10-28 21:57:02 -0700

    completed wave 1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,6 @@
-TASKS = [
-    {name: "Organize study notes",
-    description: "notes should be argonized by subject",
-    completed: ""},
-    {name: "Finalize study notes",
-        description: "notes should be finalized by study buddies",
-        completed: "done!"}
-]
 
 class TasksController < ApplicationController
     def index
-        @tasks =TASKS
+        @tasks =Task.all
     end
 end
