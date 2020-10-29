951cbc55e8cd1ecb70dec8b45059bbbd5c4a708b
KayKay-git <kayalew@yahoo.com>
2020-10-29 19:17:21 -0400

    WAVE 1 complete, passes test, started WAVE 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,21 @@
-TASKS = ['eat', 'code', 'sleep']
+# TASKS = [
+#     { name:'create', description: 'fun video', completed_at: nil},
+#     { name: 'study', description: 'week 1 rails', completed_at: nil},
+#     { name: 'exercise', description: 'attend virtual yoga', completed_at: nil}
+# ]
 
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
+
+  def show
+    task_id = params[:id].to_i
+    @task = @tasks[task_id]
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,11 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+require 'minitest/rails'
+require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
