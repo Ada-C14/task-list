ad150528fcfe82e4b77d1578124fe30bfcd9bfe2
Christabel Sebastian <christabel.escarez@gmail.com>
2020-10-29 11:10:15 -0700

    Defined #new

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,18 @@
 class TasksController < ApplicationController
+
   def index
     @tasks = Task.all
   end
+
   def show
     task_id = params[:id].to_i
     @task = Task.find_by(id: task_id)
 
     return head :not_found if @task.nil?
   end
+
+  def new
+    @book = Book.new
+  end
+
 end
