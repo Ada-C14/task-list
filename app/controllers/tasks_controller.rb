204dd0bf94d0a2a707276a892a3c9e1d3525de88
r-spiel <richelle.mehlhaff@gmail.com>
2020-10-28 18:08:08 -0700

    wave 1 - update controller and view to show the data from db

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,6 @@
-TASKS = ["get groceries", "take cat to the vet", "take out the trash"]
-
-
 class TasksController < ApplicationController
   #controller Actions are always methods!
   def index #index means list all
-    @tasks = TASKS
+    @tasks = Task.all
   end
 end
