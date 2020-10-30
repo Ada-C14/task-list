560d0db2f8bc8ba065c1e5c8c2a94f8d99b253c5
Ren Carothers <rencarothers@gmail.com>
2020-10-29 19:46:18 -0700

    preliminarily fleshed out new, create, edit, update methods and .html.erb pages for new and edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,11 +19,49 @@ class TasksController < ApplicationController
         end    
     end
 
-    def update
-    end
-    
+    def new
+        @task = Task.new
+    end    
+
+    def create
+        @task = Task.new(
+            name: params[:task][:name], 
+            description: params[:task][:description], 
+            completed_at: params[:task][:completed_at]
+            )
+        
+        if task.save
+            redirect_to tasks_path
+        else
+            render :new, :bad_request
+            return
+        end 
+    end               
+
     def edit
+        task_id = params[:id]
+        @task = Task.find_by(id: task_id)
+
+        if @task.nil?
+            head :not_found
+            return   
+        end             
     end
+
+    def create
+        @task.update(
+            name: params[:task][:name], 
+            description: params[:task][:description], 
+            completed_at: params[:task][:completed_at]
+            )
+        
+        if @task.update
+            redirect_to task
+        else
+            render :edit, :bad_request
+            return
+        end   
+    end    
     
     def destroy
     end    
