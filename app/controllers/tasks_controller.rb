2fdea58cd35cc709325b67056f256f1a43696ea3
Schanen Ryan <schanen.r@gmail.com>
2020-10-29 18:19:22 -0700

    task list linked in index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,4 +11,34 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    task_id = params[:id].to_i
+    @task = Task.find(task_id)
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def update
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
 end
