6ceb61029e4b33f11e732b4e3930fe727fa14fc0
Yingjing Lin <yjlin789@gmail.com>
2020-10-29 22:47:04 -0700

    complete Task#show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,16 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+    if @task.nil?
+      redirect_to tasks_path
+      return
+    end
+  end
+
+
+
 end
