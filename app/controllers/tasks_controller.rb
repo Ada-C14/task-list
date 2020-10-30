01f687b7393a3d6b04aa8e805a3033e92dacc530
Ida <ida.goitom@yahoo.com>
2020-10-29 21:26:43 -0700

    updated the create and new methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,11 +8,42 @@ class TasksController < ApplicationController
   end
 
   def show
+
     task_id = params[:id].to_i
-    @task = Task.find(task_id)
+    @task = Task.find_by(id: task_id)
     if @task.nil?
-      head :not_found
-      return
+       redirect_to root_path
+    end
+
+
+  end
+
+  def update
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(name: params[:task][:name],
+                     description: params[:task][:description],
+                     completed_at: params[:task][:completed_at]) #instantiate a new book
+    if @task.save # save returns true if the database insert succeeds
+      redirect_to task_path(@task.id) # go to the index so we can see the task in the list
+      #
+    else # save failed :(
+    render :new, :bad_request # show the new task form view again
+
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
-  get '/tasks', to: 'tasks#index'
-  get '/tasks/:id', to: 'tasks#show'
+  # Add routes that have to do with a collection of books
+  get '/tasks', to: 'tasks#index', as: 'tasks' #lists all tasks
+  get '/tasks/new', to: 'tasks#new', as: 'new_task' #gets a form for a new task
+  post '/tasks', to: 'tasks#create'
+  #Add routes that deal with a specific Task
+  get '/tasks/:id', to: 'tasks#show', as: 'task' #shows details for 1 task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task' #brings up the form to edit a task
+
 end
