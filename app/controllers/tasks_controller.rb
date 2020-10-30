58ad5a56a75204f8c0215a28a698029e921bf2b6
Pauline Chane <pauline.chane@gmail.com>
2020-10-29 19:26:07 -0700

    added option and form to create new task, added some more html skeletons for future functionality, wave 2 complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,12 +12,16 @@ class TasksController < ApplicationController
     end
   end
 
+  def new
+    @task = Task.new
+  end
+
   def create
     @task = Task.new(name: params[:task][:name],
                      description: params[:task][:description],
                      completed_at: params[:task][:completed_at]) #instantiate a new book
     if @task.save # save returns true if the database insert succeeds
-      redirect_to root_path # go to the index so we can see the book in the list
+      redirect_to task_path(@task.id) # go to the index so we can see the book in the list
       return
     else # save failed :(
     render :new # show the new book form view again
