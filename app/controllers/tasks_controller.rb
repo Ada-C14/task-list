c686a7789d9efcbfc2dcdb80e4d1745157db6fe6
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2020-10-29 23:50:39 -0700

    Wave 2 all tests passing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,35 @@
-# TASKS = [
-#   { description:  "repot plants", due: "Monday" },
-#   { description: "eat donut", due: "Tuesday" },
-#   { description:  "attend yoga", due: "Wednesday" }
-# ]
-
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
+  end
+
+  def show
+    id = params[:id].to_i
+    # if Task.find(id).nil?
+    #   flash[:error] = "Item not found"
+    # else
+    @task = Task.find(id)
+    # end
+
+    # redirect_to tasks_path if @task.nil?
+
+    # head :redirect if @task.nil?
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(
+      name: params[:task][:name],
+      description: params[:task][:description],
+      completed_at: params[:task][:completed_at])
+
+    if @task.save
+      redirect_to task_path(@task.id)
+    else
+      render :new, :bad_request
+    end
   end
 end
