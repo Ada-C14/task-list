383c18dd2367ba149dc289f3cd9272b8c0bdbfb4
Christabel Sebastian <christabel.escarez@gmail.com>
2020-10-29 17:31:36 -0700

    Can add new tasks and bring user to new page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,11 +8,57 @@ class TasksController < ApplicationController
     task_id = params[:id].to_i
     @task = Task.find_by(id: task_id)
 
-    return head :not_found if @task.nil?
+    if @task.nil?
+      return head :not_found
+    end
+
   end
 
   def new
-    @book = Book.new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(
+        name: params[:task][:name],
+        description: params[:task][:description],
+        completed_at: params[:task][:completed_at]
+    )
+    if @task.save # save returns true if the database insert succeeds
+      redirect_to task_path(@task) # go to the index so we can see the task in the list
+      return
+    else # save failed :(
+      render :new # show the new task form view again
+      return
+    end
+  end
+
+  def edit
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+
+    if @task.nil?
+      return head :not_found
+    end
+  end
+
+  def update
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+
+    if @task.nil?
+      return head :not_found
+    elsif @task.update(
+      name: params[:task][:name],
+      description: params[:task][:description],
+      completed_at: params[:task][:completed_at]
+    )
+      redirect_to task_path(@task)
+      return
+    else
+      render :edit
+      return
+    end
   end
 
 end
