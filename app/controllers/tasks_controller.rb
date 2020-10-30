d9c10847f5a85c6d17e97316d3616738d0142a67
Iris Lux <iris.lux0@gmail.com>
2020-10-29 21:04:40 -0700

    new/create, update/edit actions added to controller, views added as well, tests added for all new actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,25 +9,41 @@ class TasksController < ApplicationController
     task_id = params[:id].to_i
     @task = Task.find_by(id: task_id)
     if @task.nil?
-      render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found
-      return
+      redirect_to tasks_path
     end
   end
 
   def create
+    task = Task.new(name: params[:task][:name], description: params[:task][:description])
 
+    if task.save
+      redirect_to task_path(task.id)
+    else
+      render :new, bad_request
+    end
   end
 
   def new
-
+    @task = Task.new
   end
 
   def edit
-
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: task_id)
+    if @task.nil?
+      redirect_to tasks_path
+    end
   end
 
   def update
-
+    task = Task.find_by(id: params[:id].to_i)
+    if task.nil?
+      redirect_to tasks_path
+      elsif task.update name: params[:task][:name], description: params[:task][:description]
+      redirect_to task_path
+    else
+      render :new, bad_request
+    end
   end
 
   def destroy
