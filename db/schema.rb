9f39eed781a52d0ac86cb66abf51939aa901cbb9
Christabel Sebastian <christabel.escarez@gmail.com>
2020-10-31 16:29:34 -0700

    ACTUALLY got a successful migration to change the completed_at data type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_175209) do
+ActiveRecord::Schema.define(version: 2020_10_31_222732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_10_29_175209) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.string "completed_at"
+    t.datetime "completed_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
