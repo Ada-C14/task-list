87cac723f5c1d4fd7badf1aadffa0650e8906b53
Jasmine <jasylop@gmail.com>
2020-10-31 16:34:56 -0700

    added new & create actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,13 +7,24 @@ class TasksController < ApplicationController
     id = params[:id].to_i
     @task = Task.find_by(id: id)
     if @task.nil?
-      head :not_found
+      redirect_to tasks_path
       return
     end
   end
 
   def new
-    
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description], completed_at: params[:task][:completed_at])
+    if @task.save #save returns true if successful
+      redirect_to task_path(@task.id)
+      return
+    else
+      redirect_to :new
+      return
+    end
   end
 
 
