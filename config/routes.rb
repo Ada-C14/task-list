20eb793f03f05cfb5ba6a52ac13c19b21b1f6529
Jasmine <jasylop@gmail.com>
2020-10-31 16:32:53 -0700

    Updated with resources && toggle_complete path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-  get '/tasks', to: 'tasks#index', as: 'tasks'
-  get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
-
+  resources :tasks, except: [:put]
+  patch '/tasks/:id/toggle_complete', to: 'tasks#toggle_complete', as: 'toggle_complete'
 end
