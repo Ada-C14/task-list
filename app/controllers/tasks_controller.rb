70cae48f12d32aa89cc1b61f66a55c5aa7c668d5
roshni-patel <rpatel01@wesleyan.edu>
2020-10-31 15:07:48 -0500

    Added strong params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,7 @@ class TasksController < ApplicationController
         task_id = params[:id].to_i
         @task = Task.find_by(id: task_id)
         if @task.nil?
-          redirect_to action: "index" 
+          redirect_to root_path
           return
         end
     end
@@ -17,8 +17,7 @@ class TasksController < ApplicationController
     end
 
     def create
-        @task = Task.new(name: params[:task][:name],
-            description: params[:task][:description]) 
+        @task = Task.new(task_params) 
 
         if @task.save 
             redirect_to task_path(@task.id)
@@ -44,11 +43,7 @@ class TasksController < ApplicationController
         if @task.nil?
           redirect_to root_path
           return
-        elsif @task.update(
-          name: params[:task][:name], 
-          description: params[:task][:description], 
-          completed_at: params[:task][:completed_at])
-
+        elsif @task.update(task_params)
           redirect_to task_path(@task.id)
           return
         else 
@@ -85,4 +80,10 @@ class TasksController < ApplicationController
 
         redirect_to root_path
     end
+end
+
+private
+
+def task_params
+  return params.require(:task).permit(:name, :description, :completed_at)
 end
