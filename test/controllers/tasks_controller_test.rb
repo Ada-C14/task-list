69aacacbdacd03e316b7a6225c31cc80d4098e62
gessica <gessicamohr18@gmail.com>
2020-10-31 16:06:32 -0700

    Created tests for edit action and update action, it has and edit link and page and after editing takes you to the show page. Tests for Wave 3 passed.

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -88,13 +88,17 @@ describe TasksController do
   # Unskip and complete these tests for Wave 3
   describe "edit" do
     it "can get the edit page for an existing task" do
-      skip
+      #skip
       # Your code here
+        get edit_task_path(task.id)
+        must_respond_with :success
     end
     
     it "will respond with redirect when attempting to edit a nonexistant task" do
-      skip
+      #skip
       # Your code here
+      get edit_task_path(-1)
+      must_respond_with :redirect
     end
   end
   
@@ -102,12 +106,44 @@ describe TasksController do
   describe "update" do
     # Note:  If there was a way to fail to save the changes to a task, that would be a great
     #        thing to test.
-    it "can update an existing task" do
       # Your code here
+      before do
+        Task.create(name: "Testing", description: " Testing description", completed_at: "Testing completes task")
+      end
+      let (:new_task_hash) {
+        {
+            task: {
+                name: "Make beds",
+                description: "Make all 3 beds",
+                completed_at: "Everyday",
+            },
+        }
+      }
+    it "can update an existing task" do
+        id = Task.first.id
+        expect {
+          patch task_path(id), params: new_task_hash
+        }.wont_change "Task.count"
+
+        must_redirect_to tasks_path
+
+        task = Task.find_by(id: id)
+        expect(task.name).must_equal new_task_hash[:task][:name]
+        expect(task.description).must_equal new_task_hash[:task][:description]
+        expect(task.completed_at).must_equal new_task_hash[:task][:completed_at]
+
     end
     
     it "will redirect to the root page if given an invalid id" do
       # Your code here
+      id = -1
+
+      expect {
+        patch task_path(id), params: new_task_hash
+      }.wont_change "Task.count"
+
+      must_respond_with :redirect
+
     end
   end
   
