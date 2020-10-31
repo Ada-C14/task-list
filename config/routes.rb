a1ea07a96eff6457dac0197646a1f8c855cb5088
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-10-30 20:49:19 -0700

    edit typo

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
 
   get '/tasks/:id', to: 'tasks#show', as: 'task'  # shows details for one task
   get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'  # brings up the form to edit a task
-  patch '/tasks/:id', to: 'tasks#update', as: 'edit_task'  # update an existing task
+  patch '/tasks/:id', to: 'tasks#update'  # update an existing task
   delete '/tasks/:id', to: 'tasks#destroy'  # destroy a given task
 end
