f237cecd35d1de20e0bccdf95cc367a4e8019298
beauttie <bak02013@pomona.edu>
2020-11-01 00:21:58 -0700

    Add new and create actions to controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,11 +16,26 @@ class TasksController < ApplicationController
   end
 
   def show
-    task_id = params[:id].to_i
-    @task = Task.find_by(id: task_id)
+    @task = Task.find_by(id: params[:id])
 
     if @task.nil?
       head :not_found
+      return
+    end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description], completed_at: params[:task][:completed_at])
+
+    if @task.save
+      redirect_to task_path(@task.id)
+      return
+    else
+      render :new
       return
     end
   end
