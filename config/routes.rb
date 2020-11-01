84839711471437f126f096fe8c47b61778e04bdd
Anna Kim <anna.kim93@gmail.com>
2020-10-31 23:03:50 -0400

    updated routes with resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,7 @@ Rails.application.routes.draw do
   # verb 'path', to: 'controller#action'
 
   root 'tasks#index'
-
-  # Routes that operate on tasks + routes that have to come before others
-  get '/tasks', to: 'tasks#index', as: :tasks
-  get '/tasks/new', to: 'tasks#new', as: :new_task
-  post '/tasks', to: 'tasks#create'
-
-  # Routes that operate on an individual task
-  get '/tasks/:id', to: 'tasks#show', as: :task
-  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch '/tasks/:id', to: 'tasks#update'
-  delete '/tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 
   patch 'tasks/:id/mark_complete', to: 'tasks#mark_complete', as: :task_complete
   patch 'tasks/:id/mark_incomplete', to: 'tasks#mark_incomplete', as: :task_incomplete
