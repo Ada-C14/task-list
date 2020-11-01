685f47c107a3602afd9304ec5727a783146d9911
beauttie <bak02013@pomona.edu>
2020-11-01 01:54:52 -0800

    Test edit and update actions

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -88,13 +88,18 @@ describe TasksController do
   # Unskip and complete these tests for Wave 3
   describe "edit" do
     it "can get the edit page for an existing task" do
-      skip
-      # Your code here
+      # skip
+      get edit_task_path(task.id)
+
+      must_respond_with :success
     end
     
-    it "will respond with redirect when attempting to edit a nonexistant task" do
-      skip
-      # Your code here
+    it "will respond with redirect when attempting to edit a nonexistent task" do
+      # skip
+      get edit_task_path(-1)
+
+      # Assert
+      must_respond_with :redirect
     end
   end
   
@@ -103,11 +108,36 @@ describe TasksController do
     # Note:  If there was a way to fail to save the changes to a task, that would be a great
     #        thing to test.
     it "can update an existing task" do
-      # Your code here
+      get edit_task_path(task.id)
+
+      edited_task_hash = {
+        task: {
+          name: "first task of the day",
+          description: "wake up",
+          completed_at: Time.parse("9:00"),
+        }
+      }
+
+      # Act-Assert
+      expect {
+        patch task_path(task.id), params: edited_task_hash
+      }.wont_change "Task.count"
+
+      edited_task = Task.find_by(id: task.id)
+      expect(edited_task.name).must_equal edited_task_hash[:task][:name]
+      expect(edited_task.description).must_equal edited_task_hash[:task][:description]
+      expect(edited_task.completed_at).must_equal edited_task_hash[:task][:completed_at]
+
+      must_respond_with :redirect
+      must_redirect_to task_path(task.id)
     end
     
     it "will redirect to the root page if given an invalid id" do
-      # Your code here
+      patch task_path(-1)
+
+      # Assert
+      must_respond_with :redirect
+      must_redirect_to root_path
     end
   end
   
