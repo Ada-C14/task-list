e5a9aad126bf8f8a722269b073cce83e316d5b46
Kalkidan Ashenafi <kalkidanashenafi@gmail.com>
2020-10-31 18:35:05 -0700

    Wave 1: Task Model done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,9 @@
-TASKS = [{id: 0, name: "Unit 2: Ruby on Rails", description: "learn rails", due_date: "10-20-2020"}]
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
 
- class TasksController < ApplicationController
-   def index
-     @tasks = TASKS
-   end
-
-   def show
-     @task = TASKS[0]
-   end
- end
+#   def show
+#     @task = TASKS[0]
+#   end
+end
