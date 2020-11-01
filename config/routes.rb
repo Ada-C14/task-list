65783dfe0010edee56e518f9ec64851cffb8e2ea
Kalkidan Ashenafi <kalkidanashenafi@gmail.com>
2020-10-31 19:29:25 -0700

    Wave 1 Tests Passing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # # Routes concerned with task collection
+  root 'tasks#index'
   get '/tasks/', to: 'tasks#index', as: 'tasks'
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
   post '/tasks', to: 'tasks#create'
