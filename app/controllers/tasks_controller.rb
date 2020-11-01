a90f177d64407c830566170e022f083d264e8a2a
beauttie <bak02013@pomona.edu>
2020-10-31 22:53:19 -0700

    Create database and update index action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,17 @@
-TASKS = [
-    { task: "review notes from the beginning of time" },
-    { task: "clean my room dammit" },
-    { task: "code a pretty website for once" },
-    { task: "cuss at my computer less and just code better" },
-    { task: "fix my dysfunctional sleeping schedule" },
-    { task: "go outside and enjoy nature while it lasts" },
-    { task: "eat more veggies and drink more water" },
-    { task: "stop procrastinating HAHA jk as if that'll ever happen" },
-    { task: "get my shit together x100 on god" }
-]
+# TASKS = [
+#     { task: "review notes from the beginning of time" },
+#     { task: "clean my room dammit" },
+#     { task: "code a pretty website for once" },
+#     { task: "cuss at my computer less and just code better" },
+#     { task: "fix my dysfunctional sleeping schedule" },
+#     { task: "go outside and enjoy nature while it lasts" },
+#     { task: "eat more veggies and drink more water" },
+#     { task: "stop procrastinating HAHA jk as if that'll ever happen" },
+#     { task: "get my shit together x100 on god" }
+# ]
 
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 end
