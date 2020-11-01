d9ad19daf68b68c17c9e4b80898b95d1e66d124b
Stacy <lundquist.stacy@gmail.com>
2020-10-31 17:05:38 -0700

    Added forms

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,3 @@
-
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
@@ -15,5 +14,50 @@ class TasksController < ApplicationController
 
   def new
     @task = Task.new
+  end
+
+  def create
+    @task = Task.new(
+      name: params[:task][:name],
+      description: params[:task][:description],
+      started_at: params[:task][:started_at],
+      completed_at: params[:task][:completed_at]
+    )
+    if @task.save # save returns true if the database insert succeeds
+      redirect_to tasks_path
+      return
+    else
+      render :new
+      return
+    end
+  end
+
+  def edit
+    @task = Task.find_by(id: params[:id])
+
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+    if @task.nil?
+      head :not_found
+      return
+    elsif @task.update(
+      name: params[:task][:name],
+      description: params[:task][:description],
+      started_at: params[:task][:started_at],
+      completed_at: params[:task][:completed_at]
+    )
+      redirect_to tasks_path # go to the index so we can see the book in the list
+      return
+    else
+      # save failed :(
+      render :edit # show the new book form view again
+      return
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'tasks#index'
-  get '/tasks', to: 'tasks#index'
+  get '/tasks', to: 'tasks#index', as: 'tasks'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  get '/tasks/show', to: 'tasks#show', as: 'show_task'
+  get '/tasks/edit', to: 'tasks#edit', as: 'edit_task'
 end
