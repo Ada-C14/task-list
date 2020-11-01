a2cce01630f947255095fee41551138ffd3ba912
Stacy <lundquist.stacy@gmail.com>
2020-10-31 17:41:16 -0700

    updated #show for tests

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,7 @@ class TasksController < ApplicationController
     task_id = params[:id]
     @task = Task.find_by(id: task_id)
     if @task.nil?
-      head :not_found
+      redirect_to tasks_path
       return
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'tasks#index'
   get '/tasks', to: 'tasks#index', as: 'tasks'
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  get '/tasks/show', to: 'tasks#show', as: 'show_task'
-  get '/tasks/edit', to: 'tasks#edit', as: 'edit_task'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
 end
