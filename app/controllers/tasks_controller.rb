5c95e2464c4e75031197135424260fefbf039193
beauttie <bak02013@pomona.edu>
2020-10-31 23:16:54 -0700

    Add show action to controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,4 +14,14 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: task_id)
+
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
 end
