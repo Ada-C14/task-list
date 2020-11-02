bec49ab48203989e9fe67a8e1320c8d3e605485a
Lina Do <l.do5147@gmail.com>
2020-11-02 00:09:30 -0800

    created the show method in the tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,13 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def show
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
+
 end
