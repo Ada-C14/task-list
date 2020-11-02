56a94ec19a0379c2bacc33c3dcb8308faff0ca3d
Leah Scott-Zechlin <scottzec@gmail.com>
2020-11-01 21:33:10 -0800

    show each task route, controller action and erb view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,5 +4,24 @@ class TasksController < ApplicationController
 
   def index
     @tasks = Task.all
-  end  
+  end
+
+  def show
+    task_id = params[:id] #we'll be able to access our route parameter via a special hash provided by Rails called params. The ID sent by the browser will be stored under the key :id (remember that this is the name we gave the parameter in the routefile).
+    @task = Task.find_by(id: task_id)
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
+
 end
+
+# def show
+#   book_id = params[:id]
+#   @book = Book.find_by(id: book_id)
+#   if @book.nil?
+#     head :not_found
+#     return
+#   end
+# end
