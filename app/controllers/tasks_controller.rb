63239c2e4dd0b0d659f73799b5835b2634097504
Lina Do <l.do5147@gmail.com>
2020-11-02 00:57:13 -0800

    added the create action in the Tasks controller file

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,4 +17,16 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description], completed_at: params[:task][:completed_at])
+
+    if @task.save
+      redirect_to task_path(@task.id)
+      return
+    else
+      render :new
+      return
+    end
+  end
+
 end
