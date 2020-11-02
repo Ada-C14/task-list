a320d6073fcf845838c476096e4b4234502c7924
marks214 <aimee.oz.x@gmail.com>
2020-11-01 19:41:31 -0800

    added the edit action and view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -32,5 +32,28 @@ class TasksController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @task = Task.find_by(id: params[:id])
+  end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+    result = @task.update(
+      {
+        name: params[:task][:name],
+        description: params[:task][:description],
+        completed_at: params[:task][:completed_at]
+      }
+    )
+
+    if result
+      redirect_to task_path(@task.id) # params[:id])
+    else
+      render :edit
+    end
+
+  end
+
 end
 
