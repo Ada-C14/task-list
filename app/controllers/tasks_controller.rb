ecbf95d7f88fbf62493681f507c8d36362dd6696
Sophia Donan <sophiadonan@gmail.com>
2020-11-01 23:57:27 -0800

    Completed Wave 1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,7 @@
-TASKS = [
-    { item: "Make Bed", status: "Complete"},
-    { item: "Clean Litter Box", status: "Not-complete"},
-    { item: "Do Dishes", status: "Not-complete"},
-    { item: "Vaccuum Rug", status: "Not-complete"},
-    { item: "Fold Laundry", status: "Not-complete"}
-]
+
+
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 end
