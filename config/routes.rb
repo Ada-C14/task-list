1ea8ae48583c7d265e633005a6be0c844d8aff9e
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-11-01 17:09:25 -0800

    replace 7 RESTful routes with resources :tasks

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,16 @@ Rails.application.routes.draw do
 
   root to: 'tasks#index'
 
-  get '/tasks', to: 'tasks#index', as: 'tasks'  # lists all tasks
-  get '/tasks/new', to: 'tasks#new', as: 'new_task'  # Gets a form for a new task
-  post 'tasks', to: 'tasks#create'  # create a task
-
-  get '/tasks/:id', to: 'tasks#show', as: 'task'  # shows details for one task
-  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'  # brings up the form to edit a task
-  patch '/tasks/:id', to: 'tasks#update'  # update an existing task
+  resources :tasks
   patch '/tasks/:id/toggle_complete', to: 'tasks#toggle_complete', as: 'toggle_complete_task'  # update an existing task
-  delete '/tasks/:id', to: 'tasks#destroy'  # destroy a given task
+
+  # get '/tasks', to: 'tasks#index', as: 'tasks'  # lists all tasks
+  # get '/tasks/new', to: 'tasks#new', as: 'new_task'  # Gets a form for a new task
+  # post 'tasks', to: 'tasks#create'  # create a task
+  #
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'  # shows details for one task
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'  # brings up the form to edit a task
+  # patch '/tasks/:id', to: 'tasks#update'  # update an existing task
+  # patch '/tasks/:id/toggle_complete', to: 'tasks#toggle_complete', as: 'toggle_complete_task'  # update an existing task
+  # delete '/tasks/:id', to: 'tasks#destroy'  # destroy a given task
 end
