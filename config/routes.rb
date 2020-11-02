7f3bbe260a48be0c26a4a21b6ef73c9173523e85
Ringo Alcock <ringo.alcock@gmail.com>
2020-11-02 10:03:34 -0800

    clean up routes with resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
 
-  # verb 'path', to: 'controller#action'
-  get '/tasks', to: 'tasks#index', as: 'tasks'
-  get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  post '/tasks/', to: 'tasks#create'
-
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
-  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   patch '/tasks/:id/toggle', to: 'tasks#toggle_complete', as: 'toggle_task'
-  patch '/tasks/:id', to: 'tasks#update'
-  delete '/tasks/:id', to: 'tasks#destroy'
-
   root to: 'tasks#index'
+  resources :tasks
+
 end
