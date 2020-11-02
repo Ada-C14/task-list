edc1e400ded603462693eb1f6ccd9a6d54224f3b
Sophia Donan <sophiadonan@gmail.com>
2020-11-02 14:23:06 -0800

    Wave 2 complete and all tests passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index', as: 'tasks'
   root to: 'tasks#index'
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
   post '/tasks/', to: 'tasks#create'
 end
