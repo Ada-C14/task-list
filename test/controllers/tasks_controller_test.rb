66125c43022ec2a655de9f54a76440f4cf87aea5
Sophia Donan <sophiadonan@gmail.com>
2020-11-02 14:38:41 -0800

    Wave 3: created edit test

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -79,31 +79,31 @@ describe TasksController do
     end
   end
   
-  # # Unskip and complete these tests for Wave 3
-  # describe "edit" do
-  #   it "can get the edit page for an existing task" do
-  #     skip
-  #     # Your code here
-  #   end
+  # Unskip and complete these tests for Wave 3
+  describe "edit" do
+    it "can get the edit page for an existing task" do
+      get edit_task_path(task.id)
+      must_respond_with :success
+    end
     
-  #   it "will respond with redirect when attempting to edit a nonexistant task" do
-  #     skip
-  #     # Your code here
-  #   end
-  # end
+    it "will respond with redirect when attempting to edit a nonexistant task" do
+      get edit_task_path(-1)
+      must_respond_with :redirect
+    end
+  end
   
-  # # Uncomment and complete these tests for Wave 3
-  # describe "update" do
-  #   # Note:  If there was a way to fail to save the changes to a task, that would be a great
-  #   #        thing to test.
-  #   it "can update an existing task" do
-  #     # Your code here
-  #   end
+  # Uncomment and complete these tests for Wave 3
+  describe "update" do
+
+    it "can update an existing task" do
+
+
+    end
     
-  #   it "will redirect to the root page if given an invalid id" do
-  #     # Your code here
-  #   end
-  # end
+    it "will redirect to the root page if given an invalid id" do
+      # Your code here
+    end
+  end
   
   # # Complete these tests for Wave 4
   # describe "destroy" do
