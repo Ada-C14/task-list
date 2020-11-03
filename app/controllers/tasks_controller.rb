0c976f6a011616a1166863a4ff8f93393456fe1f
Jessica Chan <jwinchan@gmail.com>
2020-11-03 02:01:09 -0800

    created edit form and edit/update methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,19 +16,46 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
-  def update
-
+  def create
+    @task = Task.new(
+        name: params[:task][:name],
+        description: params[:task][:description]
+    )
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new, :bad_request
+    end
   end
 
   def edit
+    @task = Task.find_by(id: params[:id])
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
 
+  def update
+    @task = Task.find_by(id: params[:id])
+    if @task.nil?
+      head :not_found
+      return
+    elsif @task.update(
+        name: params[:task][:name],
+        description: params[:task][:description],
+        completed_at: params[:task][:completed_at]
+    )
+      redirect_to tasks_path
+      return
+    else
+      render :edit, :bad_request
+      return
+    end
   end
 
   def destroy
 
   end
 
-  def create
-
-  end
 end
