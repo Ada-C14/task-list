a1356d6bd70baa2bfd7c2517fdcd33bae3bf115c
Kalkidan Ashenafi <kalkidanashenafi@gmail.com>
2020-11-02 18:06:56 -0800

    Added partial views

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,11 @@ require("channels")
 
 import "bootstrap"
 
-
+console.log('Hello from application.js')
+$("li").click(function(event){
+  $("li").removeClass("selected");
+	$(this).addClass("selected");
+});
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
