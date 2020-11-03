f01cc3f1fcb73444e4c62ea473c14451880fc335
Marj E <marj@marjorieetienne.com>
2020-11-03 08:45:13 -0800

    completed wave 3 and part 1 of wave 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
 
-  # Routes that operate on the task collection
-  get '/tasks', to: 'tasks#index', as: 'tasks'
-  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  # # Routes that operate on the task collection
+  # get '/tasks', to: 'tasks#index', as: 'tasks'
+  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
   # post '/tasks', to: 'tasks#create'
-
-  # Routes that operate on individual tasks
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  #
+  # # Routes that operate on individual tasks
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
   # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   # patch '/tasks/:id', to: 'tasks#update'
   # delete'/tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
 
 end
