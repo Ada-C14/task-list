b593dcbd743e338a52f06c1c211ae6ad36217bd1
Jessica Chan <jwinchan@gmail.com>
2020-11-02 21:33:02 -0800

    got rid of data constant and added links to show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,8 +8,7 @@ class TasksController < ApplicationController
     task_id = params[:id].to_i
     @task = Task.find_by(id: task_id)
     if @task.nil?
-      head :not_found
-      return
+      redirect_to tasks_path
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'tasks#index'
+
   get '/tasks', to: 'tasks#index', as: 'tasks'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
 end
