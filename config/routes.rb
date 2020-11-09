2f41e1349b2b031498eadde9b7d5165b07251942
Madeline <madelign@gmail.com>
2020-11-09 17:28:21 -0500

    finished create task and passed wave 2 tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get '/tasks', to: 'tasks#index', as: 'tasks'
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  # get '/tasks', to: 'tasks#index', as: 'tasks'
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
+  root to: "tasks#index"
+
+  # post '/tasks', to: 'tasks#create'
+  resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
